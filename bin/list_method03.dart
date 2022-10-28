0df0b0c4f132c5d9efa80597fc81865764641202
mehroj <mehrojgaybullayev49@gmail.com>
2022-10-28 15:23:01 +0500

    033

--- bin/list_method03.dart
+++ bin/list_method03.dart
@@ -7,11 +7,11 @@
     Returns:
         list: return answer
 */
-List function(List fruits1, List fruits2) {
+List func(List fruits1, List fruits2) {
   fruits1.addAll(fruits2);
   return fruits1;
 }
 
 void main() {
-  print(function(['olma', 'banan'], ['kiwi', 'olcha']));
+  print(func(['olma', 'banan'], ['kiwi', 'olcha']));
 }
